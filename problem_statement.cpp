6e329ad28916c396540998713013b0456d66c9a0
Onkar Swami <68021157+omiswami99@users.noreply.github.com>
2023-09-15 14:34:40 +0530

    Add logic for turn, move and angle in class

--- problem_statement.cpp
+++ problem_statement.cpp
@@ -19,7 +19,47 @@ public:
         };
     }
 
-   };
+    void move(char instruction) {
+        if (instruction == 'f') {
+            if (dir == "N") y += 1;
+            else if (dir == "S") y -= 1;
+            else if (dir == "E") x += 1;
+            else if (dir == "W") x -= 1;
+            else if (dir == "Up") z += 1;
+            else if (dir == "Down") z -= 1;
+            
+        } else if (instruction == 'b') {
+            if (dir == "N") y -= 1;
+            else if (dir == "S") y += 1;
+            else if (dir == "E") x -= 1;
+            else if (dir == "W") x += 1;
+            else if (dir == "Up") z -= 1;
+            else if (dir == "Down") z += 1;
+        }
+    }
+
+    void turn(char instruction) {
+        if (instruction == 'l') {
+            dir = changingDir[dir][0];
+        } else if (instruction == 'r') {
+            dir = changingDir[dir][1];
+        }
+    }
+
+    void angle(char instruction) {
+        if (instruction == 'u') {
+            if (dir == "N" || dir == "S") dir = "Up";
+            else if (dir == "Up") dir = "S";
+            else if (dir == "Down") dir = "N";
+        
+        } else if (instruction == 'd') {
+            if (dir == "N" || dir == "S") dir = "Down";
+            else if (dir == "Up") dir = "N";
+            else if (dir == "Down") dir = "S";
+        }
+    }
+    
+};
 int main(){
 
   return 0;
